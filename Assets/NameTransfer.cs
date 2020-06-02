b78dd9c7f19735d376d39e895c88d16674ced79b
Theodore JL Farrell <theo@farrell99.plus.com>
2020-06-02 14:14:46 +0100

    Initial Commit

---
+++ Assets/NameTransfer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class NameTransfer : MonoBehaviour
+{
+    public string theNumberString;
+
+    [SerializeField] float multiplier = 2f;
+
+    float theResultFloat;
+
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    bool isValidInput = true;
+
+    private void Update()
+    {
+        CheckValidInput();
+    }
+
+    private void CheckValidInput()
+    {
+        if (theNumberString is string) //todo wont work as "1" is string too
+        {
+            StoreName();
+        }
+        else
+        {
+            isValidInput = false;
+        }
+    }
+
+    private void StoreName()
+    {
+        theNumberString = inputField.GetComponent<Text>().text;
+    }
+
+    public void MultiplyValues() //initiates on enter
+    {
+        if (isValidInput) //todo fix
+        {
+            for(int i = 0; i < 3; i++) // todo make cleaner, just covers  2-click bug right now
+            {
+                float theNumberFloat = float.Parse(theNumberString);
+                string theResultString = theResultFloat.ToString();
+
+                theResultFloat = theNumberFloat * multiplier;
+
+                textDisplay.GetComponent<Text>().text = theResultString;
+            }  
+        }
+    }
+}
---
+++ Assets/displayText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class displayText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RespondToInput();
+    }
+
+    private void RespondToInput()
+    {
+
+    }
+}
