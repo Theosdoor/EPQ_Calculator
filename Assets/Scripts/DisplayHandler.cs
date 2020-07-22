b1fcd9bde08447582bba5d45b6c679d9f2345405
Theodore JL Farrell <theo@farrell99.plus.com>
2020-07-22 20:48:55 +0100

    button values

--- Assets/Scripts/DisplayHandler.cs
+++ Assets/Scripts/DisplayHandler.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonHandler : MonoBehaviour
+public class DisplayHandler : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/NumberButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberButton : MonoBehaviour
+{
+    public int buttonValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string buttonText = GetComponentInChildren<TextMeshProUGUI>().text;
+        buttonValue = int.Parse(buttonText);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
