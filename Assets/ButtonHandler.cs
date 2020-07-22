389d33ed91baa7541d08ec9b3f1e41656e4352c8
Theodore JL Farrell <theo@farrell99.plus.com>
2020-07-22 11:29:12 +0100

    Reset

--- Assets/ButtonHandler.cs
+++ Assets/ButtonHandler.cs
@@ -1,48 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-[DisallowMultipleComponent]
 public class ButtonHandler : MonoBehaviour
 {
-    [SerializeField] int numberInput;
 
-    private void Start()
+
+    // Start is called before the first frame update
+    void Start()
     {
-        string stringInput = gameObject.name;
-        numberInput = int.Parse(stringInput);
-        GetInput();
+        
     }
 
-    public int GetInput()
+    // Update is called once per frame
+    void Update()
     {
-        numberInput = numberInput + 1;
-        return numberInput;
-    }
-
-
-
-
-
-
-
-
-
-
-
-    //ignore below for now
-
-    private void Update()
-    {
-        ProcessInput();
-    }
-
-    private void ProcessInput ()
-    {
-        if (Input.GetKey(KeyCode.Plus))
-        {
-            print("hello");
-        }
+        
     }
 }
