f6ec713255160f8467e1703c2409ec765b6104cf
Theodore JL Farrell <theo@farrell99.plus.com>
2020-08-17 12:30:02 +0100

    Soft reset

---
+++ Assets/Scripts/CalculationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculationHandler : MonoBehaviour
+{
+    Dictionary<int, NumButton> numButtonDict = new Dictionary<int, NumButton>();
+
+    private void Start()
+    {
+        LoadButtons();
+    }
+
+    private void LoadButtons()
+    {
+        NumButton[] numButtons = FindObjectsOfType<NumButton>();
+        foreach (NumButton button in numButtons)
+        {
+            int buttonValue = button.GetButtonValue();
+            if (numButtonDict.ContainsKey(buttonValue))
+            {
+                Debug.LogWarning("Dictionary already contains a button value of " + buttonValue);
+            }
+            else
+            {
+                numButtonDict.Add(buttonValue, button);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NumButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumButton : MonoBehaviour
+{
+    int buttonValue;
+    TextMeshProUGUI buttonText;
+
+    private void Awake()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        buttonValue = int.Parse(buttonText.text);
+    }
+
+    public void OnTapped()
+    {
+        //print(buttonValue);
+
+    }
+
+    public int GetButtonValue()
+    {
+        return buttonValue;
+    }
+}
